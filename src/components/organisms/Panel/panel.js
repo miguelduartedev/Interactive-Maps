717b0545518ed1f19e2a0c064738e04b9fae672c
Luís Conceição <luisconceicao.contact@gmail.com>
2022-10-02 23:39:22 +0100

    Updated the hero, panel and profile text

--- src/components/organisms/Panel/panel.js
+++ src/components/organisms/Panel/panel.js
@@ -10,13 +10,13 @@ const Panel = () => {
           <p className="caption">Artwork generated through Midjourney A.I.</p>
         </div>
         <div className="col-12 col-lg offset-lg-1 mt-2 mt-lg-0 panel__text">
-          <h2 className="">Simple steps for map creation:</h2>
+          <h2 className="">Steps for map creation:</h2>
           <ul>
             <li>Choose one of our available maps;</li>
             <li>
               Select a color through our <b>Color Picker</b>;
             </li>
-            <li>Click on a country to apply a color to them;</li>
+            <li>Click on the countries that you want to apply the color to;</li>
             <li>
               Alternatively select a <b>Political Block</b> or a{" "}
               <b>Geographic Region</b>;
@@ -30,8 +30,8 @@ const Panel = () => {
               and you can drag the map to change it's position;
             </li>
             <li>
-              Once you're done color coding, you can fill in the{" "}
-              <b>Map Title</b> and <b>Color Legend</b>;
+              Fill the <b>Map Title</b> and <b>Color Legend</b> fields in a way
+              that describes the data you're showcasing;
             </li>
             <li>
               Once you're done, go ahead and generate your map as a free to use
