820da344013cc837713679f435c3ad52236f6406
Luís Conceição <luisconceicao.contact@gmail.com>
2022-10-11 23:14:25 +0100

    Created Group Aggregator Checkbox

--- src/components/molecules/GroupSelectors/utils/index.js
+++ src/components/molecules/GroupSelectors/utils/index.js
@@ -3,13 +3,21 @@ import {
   updateUsedColors,
 } from "../../../../redux/mapSlice";
 import { store } from "../../../../redux/store";
+import { clearAll } from "../../../organisms/ControlPanel/utils/index";
 import {
   ClassClickHandler,
   IDClickHandler,
 } from "../../../organisms/SVGMap/utils";
 import { mapContainsID } from "../../../_common";
 
-export const groupPicker = (currentMap, group, dispatch, group_data) => {
+export const groupPicker = (
+  currentMap,
+  group,
+  dispatch,
+  group_data,
+  aggregateGroups = false
+) => {
+  !aggregateGroups && clearAll(currentMap, dispatch);
   group?.map((countryID) => {
     if (mapContainsID(countryID)) {
       IDClickHandler(
