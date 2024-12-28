40c5595ca9378f4670f515fb6854785dd87162e5
Miguel Duarte <miguelduarte.contact@gmail.com>
2024-12-28 10:24:45 +0200

    Make Interactive Maps a PWA

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-module.exports = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+})
+
+module.exports = withPWA({
+  reactStrictMode: true,
   async rewrites() {
     return [
       {
         source: "/sitemap.xml",
         destination: "/api/sitemap",
       },
-    ];
+    ]
   },
-};
+})
